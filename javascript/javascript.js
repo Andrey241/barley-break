67036b26b8017939a9b8e8bd656c40be2b4c807e
Andrey241 <Andrey75.9626@gmail.com>
2022-07-13 08:39:41 +0300

    add sound

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,82 +1,181 @@
-const field = document.querySelector(".field"),
-	cellSize = 100,
-	cells = [];
-empty = {
-	value: 0,
-	top: 0,
-	left: 0,
-};
-cells.push(empty);
+let cells = [];
+function createGame() {
+	const field = document.querySelector(".field"),
+		cell = document.querySelectorAll(".cell");
+	let cellSize = 100;
 
-function move(i) {
-	const cell = cells[i],
-		leftDiff = Math.abs(empty.left - cell.left),
-		topDiff = Math.abs(empty.top - cell.top);
-
-	if (leftDiff + topDiff > 1) {
-		return;
+	function addSize() {
+		field.style.width = `${4 * cellSize}px`;
+		field.style.height = `${4 * cellSize}px`;
+		const cell = document.querySelectorAll(".cell");
+		cell.forEach((item) => {
+			item.style.width = `${cellSize}px`;
+			item.style.height = `${cellSize}px`;
+		});
 	}
 
-	//const cell = document.createElement("div");
-	cell.element.style.left = `${cellSize * empty.left}px`;
-	cell.element.style.top = `${cellSize * empty.top}px`;
-
-	const emptyLeft = empty.left,
-		emptyTop = empty.top;
-
-	empty.left = cell.left;
-	empty.top = cell.top;
-
-	cell.left = emptyLeft;
-	cell.top = emptyTop;
-
-	const isFinished = cells.every((cell) => {
-		return cell.value === cell.top * 4 + cell.left;
-	});
-	if (isFinished) {
-		alert("win");
+	empty = {
+		value: 0,
+		top: 0,
+		left: 0,
+		i: 0,
+	};
+	if (cells.length == 0) {
+		cells.push(empty);
 	}
-	let promise = new Promise((resolve, reject) => {
-		resolve(moveSound());
-	});
 
-	promise.then(() => {
-		moveSound();
-	});
+	console.log(cells);
+	function move(i) {
+		debugger;
+		const cell = cells[i],
+			leftDiff = Math.abs(empty.left - cell.left),
+			topDiff = Math.abs(empty.top - cell.top);
 
-	// document.querySelector("audio").play();
+		if (leftDiff + topDiff > 1) {
+			return;
+		}
+
+		//const cell = document.createElement("div");
+		cell.element.style.left = `${cellSize * empty.left}px`;
+		cell.element.style.top = `${cellSize * empty.top}px`;
+
+		const emptyLeft = empty.left,
+			emptyTop = empty.top;
+
+		empty.left = cell.left;
+		empty.top = cell.top;
+
+		cell.left = emptyLeft;
+		cell.top = emptyTop;
+
+		const isFinished = cells.every((cell) => {
+			return cell.value === cell.top * 4 + cell.left;
+		});
+		if (isFinished) {
+			alert("win");
+		}
+		let promise = new Promise((resolve, reject) => {
+			resolve(moveSound());
+		});
+
+		promise.then(() => {
+			moveSound();
+		});
+
+		// document.querySelector("audio").play();
+	}
+
+	//const numbers = [...Array(15).keys()].sort(() => Math.random() - 0.5);
+	let numbers = [...Array(15).keys()];
+	console.log(numbers);
+	if (cells.length == 1) {
+		for (let i = 1; i <= 15; i++) {
+			const cell = document.createElement("div"),
+				value = numbers[i - 1] + 1;
+
+			cell.className = "cell";
+			cell.innerHTML = value;
+
+			const left = i % 4,
+				top = (i - left) / 4;
+
+			cells.push({
+				value,
+				left,
+				top,
+				element: cell,
+				i,
+			});
+
+			cell.style.left = `${cellSize * left}px`;
+			cell.style.top = `${cellSize * top}px`;
+
+			field.append(cell);
+
+			cell.addEventListener("click", () => {
+				move(i);
+			});
+		}
+	} else {
+		for (let i = 1; i <= 15; i++) {
+			const cell = document.createElement("div"),
+				value = numbers[i - 1] + 1;
+
+			cell.className = "cell";
+			cell.innerHTML = value;
+
+			const left = i % 4,
+				top = (i - left) / 4;
+
+			cell.style.left = `${cellSize * left}px`;
+			cell.style.top = `${cellSize * top}px`;
+
+			field.append(cell);
+
+			cell.addEventListener("click", () => {
+				move(cells[i].i);
+			});
+		}
+	}
+
+	addSize();
 }
-
-const numbers = [...Array(15).keys()].sort(() => Math.random() - 0.5);
-
-for (let i = 1; i <= 15; i++) {
-	const cell = document.createElement("div"),
-		value = numbers[i - 1] + 1;
-	cell.className = "cell";
-	cell.innerHTML = value;
-
-	const left = i % 4,
-		top = (i - left) / 4;
-
-	cells.push({
-		value,
-		left,
-		top,
-		element: cell,
-	});
-
-	cell.style.left = `${cellSize * left}px`;
-	cell.style.top = `${cellSize * top}px`;
-
-	field.append(cell);
-
-	cell.addEventListener("click", () => {
-		move(i);
-	});
-}
+createGame();
+//sounds
 
 function moveSound() {
 	var audio = new Audio();
 	audio.src = "https://andrey241.github.io/barley-break/audio/move.mp3";
 	audio.play();
+}
+
+//buttons
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		console.log(e.target.getAttributeNames()[0]);
+		switch (e.target.getAttributeNames()[0]) {
+			case "data-save":
+				save();
+				break;
+			case "data-load":
+				break;
+			case "data-clear":
+				break;
+			case "data-inc":
+				inc();
+				break;
+			case "data-dec":
+				break;
+			case "data-res":
+				inc();
+				break;
+			default:
+				break;
+		}
+	});
+});
+
+function save() {
+	localStorage.setItem("save", true);
+	localStorage.clear();
+	localStorage.setItem("cells", JSON.stringify(cells));
+}
+
+function load() {
+	cells = JSON.parse(localStorage.getItem("cells"));
+	createGame();
+}
+
+function clearField() {
+	document.querySelectorAll(".cell").forEach((item) => {
+		console.log(item);
+		item.remove();
+	});
+}
+
+function inc() {
+	clearField();
+	createGame();
 }
